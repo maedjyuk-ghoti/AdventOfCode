85ae5a7718e7559be8a943044be33b7b4d323fd2
Ben <s.edwin.green@gmail.com>
2025-12-09 14:30:34 -0500

    Day 4

---
+++ app/src/main/kotlin/Day04.kt
@@ -0,0 +1,63 @@
+package ghoti.maedjyuk.app
+
+object Day04 {
+    private fun parse(input: String): List<String> =
+        input.split(System.lineSeparator())
+
+    private fun <T> List<T>.cartesianProduct(other: List<T>): List<Pair<T, T>> =
+        this.flatMap { a ->
+            other.map { b ->
+                a to b
+            }
+        }
+
+    private fun List<String>.isOutOfBounds(coordinates: Pair<Int, Int>): Boolean =
+        coordinates.first < 0 ||
+                coordinates.second < 0 ||
+                coordinates.first >= this.size ||
+                coordinates.second >= this.size
+
+    private fun Int.isLessThan(other: Int): Boolean =
+        this < other
+
+    private fun List<String>.checkAround(x: Int, y: Int): Boolean =
+        listOf(x - 1, x, x + 1)
+            .cartesianProduct(listOf(y - 1, y, y + 1))
+            .filter { it != Pair(x, y) }
+            .filter { this.isOutOfBounds(it).not() }
+            .count { this[it.second][it.first] == '@' }
+            .isLessThan(4)
+
+    private fun List<String>.getAccessibleRollsOfPaper(): List<Pair<Int, Int>> =
+        this.flatMapIndexed { rowIndex, row ->
+            row.mapIndexedNotNull { colIndex, position ->
+                if (position == '@' &&
+                    this.checkAround(colIndex, rowIndex)) colIndex to rowIndex
+                else null
+            }
+        }
+
+    private fun List<String>.countAccessibleRollsOfPaper(): Int =
+        this.getAccessibleRollsOfPaper()
+            .count()
+
+    fun getRollsOfPaperCount(input: String): Int =
+        parse(input)
+            .countAccessibleRollsOfPaper()
+
+    private fun List<String>.countAccessibleRollsOfPaperComplete(): Int {
+        val accessibleRollsOfPaper = this.getAccessibleRollsOfPaper()
+        if (accessibleRollsOfPaper.isEmpty()) return 0
+
+        return this.mapIndexed { rowIndex, row ->
+            row.mapIndexed { colIndex, value ->
+                if (accessibleRollsOfPaper.contains(colIndex to rowIndex)) '.'
+                else value
+            }.joinToString(separator = "")
+        }.countAccessibleRollsOfPaperComplete() + accessibleRollsOfPaper.size
+    }
+
+    fun getRollsOfPaperCount2(input: String): Int =
+        parse(input)
+            .countAccessibleRollsOfPaperComplete()
+}
